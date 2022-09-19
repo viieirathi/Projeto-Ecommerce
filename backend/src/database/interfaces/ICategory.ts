f723d0de9ee028ca5210deab2c373cb99a461995
Thiago <thiivieira@gmail.com>
2022-09-19 12:43:21 -0300

    feat(escopo do backend): :sparkles: interfaces produtos e categorias
    criando interfaces para produtos e categorias

---
+++ backend/src/database/interfaces/ICategory.ts
@@ -0,0 +1,4 @@
+export interface ICategory {
+  name_category: string;
+  role: string;
+}
---
+++ backend/src/database/interfaces/IProduct.ts
@@ -0,0 +1,7 @@
+export interface IProduct {
+  name_product: string;
+  quantity: number;
+  price: number;
+  id_user: number;
+  name_category: string;
+}
