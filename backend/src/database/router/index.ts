caa8fe6105b0caddc29cbbdd738190269919b1e2
Thiago <thiivieira@gmail.com>
2022-09-22 15:36:46 -0300

    feat(backend): :sparkles: adiciona novas rotas de produtos e categorias

--- backend/src/database/router/index.ts
+++ backend/src/database/router/index.ts
@@ -1,10 +1,14 @@
-import { Router } from "express";
-import loginRouter from "./login";
-import userRouter from "./user";
+import { Router } from 'express';
+import categoryRouter from './category';
+import loginRouter from './login';
+import productRouter from './product';
+import userRouter from './user';
 
 const routerIndex: Router = Router();
 
-routerIndex.use('/user', userRouter)
-routerIndex.use('/login', loginRouter)
+routerIndex.use('/user', userRouter);
+routerIndex.use('/login', loginRouter);
+routerIndex.use('/product', productRouter);
+routerIndex.use('/category', categoryRouter);
 
-export default routerIndex
+export default routerIndex;
